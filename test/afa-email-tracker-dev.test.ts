2f3c1bf0b5fc249dd5cd03372965192a562caf89
Yellow987 <daryldsouza123@gmail.com>
2023-04-20 19:06:17 -0700

    Initial commit

---
+++ test/afa-email-tracker-dev.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AfaEmailTrackerDev from '../lib/afa-email-tracker-dev-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/afa-email-tracker-dev-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AfaEmailTrackerDev.AfaEmailTrackerDevStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
